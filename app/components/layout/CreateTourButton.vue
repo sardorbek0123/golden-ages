dfc4ba33a7d3b0d25ddbc326f171acba956047e1
Sardorbek <jabborovsardor21@gmail.com>
2026-01-19 02:06:00 +0500

    Refactor CreateTourButton to use NuxtLink for improved navigation

--- app/components/layout/CreateTourButton.vue
+++ app/components/layout/CreateTourButton.vue
@@ -3,9 +3,10 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <button
+  <NuxtLink
+    to="#form"
     class="px-6 py-3 rounded-full border-2 border-[#f2a900] text-[#f2a900] text-xs font-normal tracking-wide uppercase hover:bg-[#f2a900] hover:text-white transition-colors"
   >
     {{ t('header.createTour') }}
-  </button>
+  </NuxtLink>
 </template>
