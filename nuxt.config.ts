19d4a426dddd0cb6ae31cf3e5694c988fd88d239
Sardorbek <78364799+sardorbek0123@users.noreply.github.com>
2026-03-01 20:01:41 +0500

    Add Facebook domain verification and Meta Pixel tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -75,8 +75,31 @@ export default defineNuxtConfig({
         {
           name: 'keywords',
           content: 'Golden Ages of Uzbekistan, Uzbekistan, history of Uzbekistan, travel to Uzbekistan, Uzbekistan travel, Uzbekistan tours, Uzbekistan travel packages, Uzbekistan travel agency, Uzbekistan travel agency in uzbekistan, Uzbekistan travel agency in tashkent, Uzbekistan travel agency in samarkand, Uzbekistan travel agency in khiva, Uzbekistan travel agency in bukhara'
+        },
+        {
+          name: 'facebook-domain-verification',
+          content: '7duk0fno85nl9nbxfn7j17fxeurvet'
         }
       ],
+      script: [
+        {
+          innerHTML: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1607287043845190');
+fbq('track', 'PageView');`
+        }
+      ],
+      noscript: [
+        {
+          innerHTML: '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1607287043845190&ev=PageView&noscript=1">'
+        }
+      ]
     },
   },
 
